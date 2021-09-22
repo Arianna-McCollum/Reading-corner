565be55bca4fad085a3b52fd7410b4a7f0b60c4e
Electrolion <AMccollum71@gmail.com>
2021-09-22 12:03:55 -0400

    Handlebars started

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const htmlRoutes = require('./routes/html-routes.js');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const exphbs = require('express-handlebars');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', htmlRoutes);
+app.set("view engine", "hbs");
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'index'
+}));
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
+});
+
+app.get('/', (req, res) => {
+  res.render("login");
 });
