995a079350ca4e4835523c48946f337c3dd1b23e
Electrolion <AMccollum71@gmail.com>
2021-09-24 18:56:25 -0400

    added routes to register screen

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,3 +5,15 @@ router.get('/', (req, res) => {
   });
 
 module.exports = router;
+
+router.get('/login', (req, res) => {
+    res.render("login");
+  });
+
+module.exports = router;
+
+router.get('/register', (req, res) => {
+    res.render("register");
+  });
+
+module.exports = router;
