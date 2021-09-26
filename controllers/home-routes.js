9af23d38a3247f69ed7c9649bd279c18403acdd8
Electrolion <AMccollum71@gmail.com>
2021-09-25 22:02:10 -0400

    Added connect-sequelize + user-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,16 +4,21 @@ router.get('/', (req, res) => {
     res.render("login");
   });
 
-module.exports = router;
+
 
 router.get('/login', (req, res) => {
     res.render("login");
   });
 
-module.exports = router;
+
 
 router.get('/register', (req, res) => {
     res.render("register");
   });
+
+  router.get('/dashboard', (req, res) => {
+    res.render("dashboard");
+  });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,19 +4,38 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 app.use(require('./controllers/'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.set("view engine", "hbs");
 app.engine('hbs', exphbs({
   extname: 'hbs',
   defaultLayout: 'index'
 }));
+
+app.engine('handlebars', hbs.engine);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
