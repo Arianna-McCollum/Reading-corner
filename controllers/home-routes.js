fed6af416a6c4a5a7c26e1371d722ae30740ddbb
Electrolion <AMccollum71@gmail.com>
2021-09-27 13:11:03 -0400

    Routes fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,32 +1,46 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
 
 router.get('/', (req, res) => {
   res.render("login");
 });
 
-
-
-router.get('/login', (req, res) => {
+router.get('/login', (req,res) => {
   res.render("login");
-});
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  }
 
+})
 
 router.get('/register', (req, res) => {
   res.render('register');
 });
 
+router.get('/dashboard',withAuth, (req, res) => {
+  res.render('dashboard');
+});
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/dashboard/');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-})
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
