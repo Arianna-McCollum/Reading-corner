109a730c1a868e2f222b7056420c3a1698ecb093
Electrolion <AMccollum71@gmail.com>
2021-09-28 16:29:55 -0400

    delete post bug

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -5,10 +5,23 @@ async function deleteFormHandler(event) {
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE'
+      method: 'DELETE',
+      body: JSON.stringify({
+        post_id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
+
     const response = await fetch(`/api/books/${id}`, {
-      method: 'DELETE'
+      method: 'DELETE',
+      body: JSON.stringify({
+        book_id: id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
   
     if (response.ok) {
